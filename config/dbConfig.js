2ffdbf6e2dc12e7cdd4806f7541e1f4f4a1fd9c7
Avinash Bharti <abharti1111@gmail.com>
2021-12-18 11:10:02 +0530

    changed db creds

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,10 @@
 const Pool= require("pg").Pool
 
 module.exports = new Pool({
-    user: "postgres",
-    password: "8lkFaC7JG68PmEY4BSUv",
-    host: "mock-interview.cbog7vkaucki.ap-south-1.rds.amazonaws.com",
+    user: "wangjblhlglyvi",
+    password: "7156d718a43cbb5f550c5f9861f2c0fdb40df43208efe68e1d0f849aea039942",
+    host: "ec2-34-255-134-200.eu-west-1.compute.amazonaws.com",
     port: 5432,
-    database: "mockdb"
+    database: "dffi7ok6hrhu6b"
 })
 
